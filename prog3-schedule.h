086a5368eca171d016338b546657cb31d3694e73
James Truong <jatruong@pdx.edu>
2023-02-22 15:34:48 -0800

    Prog3 .h Submission

--- prog3-schedule.h
+++ prog3-schedule.h
@@ -6,23 +6,28 @@
 #include <string>
 #include <iomanip>
 
+using std::ostream;
+
 // James Truong		CS302		2-22-2023
 // Program #3		Shedule Planner
 
 /* This is the header file that will contain the clas interfaces. The base class is 
 an event, it is an abstract base class. The derived classes are different events, 
 the base class will contain virtual function to use specfic derived class methods. 
-The derived classes also have unique methods and members. */
+The derived classes may have unique methods and members. */
 
 // Class Interface(s):
 
 class event 	// Abstract base class
 {
 	public:
+		event(const string & t, const string & d, const string & l); // Parameterized constructor
+		//~event();
 		virtual int add_event() = 0;
 		virtual int edit_event() = 0;
 		virtual int display_event() = 0;
-		virtual int delete_event() = 0;
+		virtual int delete_event(const string & name) = 0;
+		friend ostream & operator << (ostream & cout, const event & src); // Output
 	//private:
 	protected:
 		string title;		// Name of event
@@ -30,32 +35,60 @@ class event 	// Abstract base class
 		string location;	// Location of event
 };
 
-class meeting : public event
+class meeting : public event		// Derived class meeting is an event
 {
 	public:
+		meeting();
+		meeting(const string & t, const string & d, const string & l); // Parameterized constructor
+		~meeting();
 		int add_event();
-		int edit_event();
-		int display_event();
-		int delete_event();
+		int edit_event(const string & t);
+		int display_event(const string & t);
+		int delete_event(const string & name);
+		friend ostream & operator << (ostream & cout, const meeting & src);
 	private:
+		string topic;		// Topic / agenda for meeting
 };
 
-class workshop : public event
+class workshop : public event		// Derived class workshop is an event
 {
 	public:
+		workshop();
+		workshop(const string & t, const string & d, const string & l); // Parameterized constructor
+		~workshop();
 		int add_event();
-		int edit_event();
-		int display_event();
-		int delete_event();
+		int edit_event(const string & t);
+		int display_event(const string & t);
+		int delete_event(const string & name);
+		int change_capacity(const int & num);
+		int change_instructor(const string & replace);
+		friend ostream & operator << (ostream & cout, const workshop & src);	// Output
 	private:
+		int capacity;		// Limited spots for workshop
+		string instructor;	// Instructor or speaker name
 };
 
-class convention : public event
+class convention : public event		// Derived class convention is an event
 {
 	public:
+		convention();
+		convention(const string & t, const string & d, const string & l); // Parameterized constructor
+		~convention();
 		int add_event();
-		int edit_event();
-		int display_event();
-		int delete_event();
+		int edit_event(const string & t);
+		int display_event(const string & t);
+		int delete_event(const string & name);
+		friend ostream & operator << (ostream & cout, const convention & src); // Output
 	private:
+		float admission;	// Admission pricing for event
+		string sponser;		// Convention sponser
 };
+/*
+class node
+{
+	public:
+		node();
+		~node();
+	private:
+		event * ptr;	
+}; */
